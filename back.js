135e266a8d7d53efcaf8cae819ace0f4d5fa8b84
dduoming <60255575+imdduoming@users.noreply.github.com>
2022-07-06 20:22:22 +0900

    qr form 변경 , url 추가

---
+++ back.js
@@ -0,0 +1,17 @@
+let domainURL= 'NewfinebackendDocker.ap-northeast-2.elasticbeanstalk.com';
+function add_attendance() {
+
+    let user_name = $('#user_name').val();
+    let phone_number=$('#user_phonenumber').val();
+    let data = {'username': user_name, 'phone': phone_number};
+    $.ajax({
+        type: "POST",
+        url: `${domainURL}/add/attendances`,
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        success: function (response) {
+            alert('메시지가 성공적으로 작성되었습니다.');
+            window.location.reload();
+        }
+    });
+}
