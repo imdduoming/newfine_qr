9b5421068ce9d09f75eb2178c78787e3b90d89d4
dduoming <60255575+imdduoming@users.noreply.github.com>
2022-07-07 12:37:21 +0900

    csrf 설정

--- back.js
+++ back.js
@@ -1,15 +1,24 @@
 let domainURL= 'https://eb.newfine.tk/';
+var token = $("input[name='_csrf']").val();
+var header = "X-CSRF-TOKEN";
+
 function add_attendance() {
 
     let user_name = $('#user_name').val();
     let data = {'studentName': user_name};
     console.log(data)
 
+
     $.ajax({
         type: "POST",
         url: domainURL+'add/attendance',
         contentType: "application/json",
+        dataType : "json",
         data: JSON.stringify(data),
+        beforeSend : function(xhr)
+        {
+            xhr.setRequestHeader(header, token);
+        },
         success: function (response) {
             console.log(data)
             alert('메시지가 성공적으로 작성되었습니다.');
