f5dca75bd5451e27cd1308f492f27bbfb090602f
dduoming <60255575+imdduoming@users.noreply.github.com>
2022-07-10 16:14:08 +0900

    큐알 라이브러리 도입 #3

--- back.js
+++ back.js
@@ -55,37 +55,3 @@ function get_myattendance(user_phone) {
     });
 }
 
-//qr코드 생성
-var qrcode = new QRCode(document.getElementById("qrcode"), {
-    //가로, 세로 높이 조절
-    width : 100,
-    height : 100
-});
-
-//input:text에 들어있는 value를 qr코드로 바꿔주는 function
-function makeCode () {
-    var elText = document.getElementById("site");
-
-    if (!elText.value) {
-        alert("Input a text");
-        elText.focus();
-        return;
-    }
-
-    qrcode.makeCode(elText.value);
-}
-
-//위에 만든 function 실행
-makeCode();
-
-//텍스트 이벤트 감지
-$("#site").
-on("blur", function () {
-    makeCode();
-}).
-on("keydown", function (e) {
-    if (e.keyCode == 13) {
-        makeCode();
-    }
-});
-
